a54fc473e274df9938b4a4f6051c4246f27e17c5
ChristianC343 <christiancontreras343@gmail.com>
2024-02-21 22:53:59 -0800

    Comments to Lecture1.cs
    Added Comments

--- Assets/Objects/Lectures/Lecture1.cs
+++ Assets/Objects/Lectures/Lecture1.cs
@@ -23,7 +23,7 @@ public class Lecture1 : MonoBehaviour
     //Quaternion podiumRotationStorage;
     float speedStorage;
 
-    public GameObject tvScreen;
+    public GameObject tvScreen; // Mapped to the TV Screen In-Game
 
     // Start is called before the first frame update
     void Start()
