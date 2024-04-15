8596c4a17bb85a6fbcb30950cb16c91116666ad7
William Phong <williamphong@users.noreply.github.com>
2024-04-15 13:33:43 -0700

    Update OverlayFeed.cs
    attempting specific day-hour calculations for orbit

--- Assets/Sandbox/Simulation/Scripts/OverlayFeed.cs
+++ Assets/Sandbox/Simulation/Scripts/OverlayFeed.cs
@@ -12,25 +12,33 @@ using TMPro;
 
 public class OverlayFeed : MonoBehaviour
 {
-        // default constants 
         double AU = 149.597870700; // in millions of km, 2013 Selected Astronomical Constants, from the online Astronomical Almanac at http://asa.usno.navy.mil/, section K
-        double a = 1.00000261 * 149.597870700; // (Standish, E. Myles; Williams, James C.. "Orbital Ephemerides of the Sun, Moon, and Planets" (PDF). International Astronomical Union Commission 4: (Ephemerides).
-        public double eccentricity = 0.01670236225492288; // Laskar 2004 solution for J2000.0 (year 0  for him)
+        double sm_axis = 1.00000261*AU; // (Standish, E. Myles; Williams, James C.. "Orbital Ephemerides of the Sun, Moon, and Planets" (PDF). International Astronomical Union Commission 4: (Ephemerides).
+        double eccentricity = 0.01670236225492288; // Laskar 2004 solution for J2000.0 (year 0  for him)
         double obliquity = 0.4090928042223415; //radians, %Laskar 2004 solution for J2000.0 (year 0  for him)
-        double precession = Mathf.PI - 1.796256991128036; //radians, Laskar 2004 solution for J2000.0 (year 0  for him); complementary angle calculated here for internal use only
+        double precession = Math.PI - 1.796256991128036; //radians, Laskar 2004 solution for J2000.0 (year 0  for him); complementary angle calculated here for internal use only
         double mean_anomaly = 90; //in degrees, CCW from perihelion, tied to time passage and via Kpler's II lawto true anomaly
-        double Fo = 1366;
-        double latitude = 43;
+        double Fo = 1366; // total solar irradance at 1 AU
+        double latitude = 43; // latitude on Earth
         double T = 365.256363; // Sidereal year length in days. (Should equinox-to-equinox year be used?)
-                        //T is prescribed a-priori, as Kepler's III Law is not in the model
-        
+                            //T is prescribed a-priori, as Kepler's III Law is not in the model
+                            // period
+
+        // VARS
+        double daylength; // day length
+        double r_vec_length, sun_decl, sol;
+        double[] season_length; // season day lengths in order of winter, spring, summer, fall
+
         int year = 2000; // Example year - user can change
         double longitudePerihelion; // longitude of perihelion
+        double longitudePerigee; // longitude of perigee
         double true_anomaly; // true anomaly
-        double t; // time
-        double winter_len, spring_len, summer_len, fall_len; // season day lengths
-        double F_bar; // insolation
-        double daylength; // day length
+        double time_elapsed; // time elapsed
+        double prec_for_orbit; // precession for orbit
+        double true_solar; // true solar longitude, in degrees 
+        double mean_solar; // mean solar longitude, in degrees
+        double mean_anomaly_equinox; // mean anomaly 
+        double time_since_perihelion; // time since perihelion
 
 
     private string textToDisplay = "";
@@ -48,111 +56,112 @@ public class OverlayFeed : MonoBehaviour
     {
         textToDisplay = "";
 
-
         textToDisplay += "Current Date: " + simuControl.dateRead();
 
         year = simuControl.getYear();
 
+        int month = simuControl.getMonth();
+
+        int day = simuControl.getDay() + simuControl.getHours() + simuControl.getSecond();
+        
+        int ndays;
+        
+        switch(month){
+            case 1:
+                ndays = 0;
+                break;
+            case 2:
+                ndays = 31;
+                break;
+            case 3:
+                ndays = 59;
+                break;
+            case 4:
+                ndays = 90;
+                break;
+            case 5:
+                ndays = 120;
+                break;
+            case 6:
+                ndays = 151;
+                break;
+            case 7:
+                ndays = 181;
+                break;
+            case 8:
+                ndays = 212;
+                break;
+            case 9:
+                ndays = 243;
+                break;
+            case 10;
+                ndays = 273;
+                break;
+            case 11:
+                ndays = 304;
+                break;
+            case 12:
+                ndays = 334;
+                break;
+            default:
+            //error
+        }
+
+        int dayofyear = ndays + (day - 1);
+
         BergerSol.CalculateOrbitalParameters(year, out eccentricity, out obliquity, out longitudePerihelion);
+
+        double omega_bar = longitudePerihelion - 180;
+        omega_bar = omega_bar < 0 ? omega_bar + 360 : omega_bar;
+        //Console.WriteLine("omega_bar " + omega_bar);
+
+        int dayofequinox = 31+28+19;
+
+        int days_since_spring = dayofyear - dayofequinox;
+        days_since_spring = days_since_spring < 0 ? days_since_spring + 365 : days_since_spring;
+
+         if(prec_for_orbit<0){
+            prec_for_orbit += 360;
+        }
+        //Console.WriteLine("prec for orbit: " + prec_for_orbit);
+
+        // determine mean anomaly of spring equinox - this will be March 20 always and will depend on precession's value;
+        KeplerianSolver.keplerian(T, eccentricity, prec_for_orbit, out time_since_perihelion, out mean_anomaly_equinox);
+        
+        time_elapsed = (days_since_spring + time_since_perihelion) % T;
+
+        mean_anomaly = 360*(time_elapsed/T);
+
+        GenOrbit.OrbitCalc(sm_axis, AU, T, eccentricity, obliquity, prec_for_orbit, mean_anomaly, Fo, latitude, out r_vec_length, out sun_decl, out sol, out season_length, out daylength, out true_anomaly);
+        
+        true_solar = (true_anomaly + omega_bar)%360;
+
         textToDisplay += "\nBerger's Solution --------------------";
         textToDisplay += "\nEccentricity (dimensionless): " + eccentricity;
         textToDisplay += "\nObliquity (degrees): " + obliquity;
         textToDisplay += "\nLongitude of Perihelion (degrees): " + longitudePerihelion;
 
-        KeplerianSolver.keplerian_inverse(eccentricity, mean_anomaly, out true_anomaly);
-        textToDisplay += "\n\nInverse Keplerian -----------------";
-        textToDisplay += "\nTrue anomaly (in radians): " + true_anomaly + " in deg: " + true_anomaly*Mathf.PI/180;
-
-        KeplerianSolver.keplerian(T, eccentricity, true_anomaly, out t, out mean_anomaly);
         textToDisplay += "\n\nKeplerian -----------------";
         textToDisplay += "\nPeriod of time elapsed since last periapsis passage: " + t;
+        textToDisplay += "\nTrue anomaly (in radians): " + true_anomaly + " in deg: " + true_anomaly*Mathf.PI/180;
         textToDisplay += "\nMean anomaly (in radians): " + mean_anomaly + " in deg: " + mean_anomaly*Mathf.PI/180;
-
-        SeasonsCalc.CalculateSeasonLengths(T, eccentricity, precession, out winter_len, out spring_len, out summer_len, out fall_len);
-
+        
         textToDisplay += "\n\nNorthern Hemisphere Seasons Lengths (days) -----------------";
-        textToDisplay += "\nSpring Length: " + spring_len;
-        textToDisplay += "\nSummer Length: " + summer_len;
-        textToDisplay += "\nFall Length: " + fall_len;
-        textToDisplay += "\nWinter Length: " + winter_len;
+        textToDisplay += "\nNorthern Hemisphere Seasons Lengths (days)"
+                                + "\n   Spring Length: " + season_length[0]
+                                + "\n   Summer Length: " + season_length[1]
+                                + "\n   Fall Length: " + season_length[2]
+                                + "\n   Winter Length: " + season_length[3];
+
+        textToDisplay += "\n\nLongitude of perigee (degrees): " + omega_bar
+                                + "\n   True longitude (degrees): " + true_solar
+                                + "\n   Mean longitude (degrees): " + mean_solar;
 
         textToDisplay += "\n\nTrue solar longitude: " + (true_anomaly + longitudePerihelion) % 360;
 
         overlay.text = textToDisplay;
 
 
-        /*
-        double position_e = true_anomaly * (Mathf.PI / 180); // for parameterizing radius-vector of Earth
-
-        double b = a * Mathf.Sqrt(1 - Mathf.Pow(eccentricity, 2)); // b = semi minor axis
-        double c = eccentricity * a; // distance from center to focus
-
-        float position = (float)precession;
-        float r = (float)(a * (1 - Mathf.Pow((float)eccentricity, 2)) / (1 + (float)eccentricity * Mathf.Cos(position)));
-        float x = (float)(r * Mathf.Cos(position));
-        float y = (float)(r * Mathf.Sin(position));
-        Vector3 r_spring = new Vector3(x, y, 0);
-
-        position = (float)precession+(float)Mathf.PI/2;
-        r = (float)(a * (1 - Mathf.Pow((float)eccentricity, 2)) / (1 + (float)eccentricity * Mathf.Cos(position)));
-        x = (float)(r * Mathf.Cos(position));
-        y = (float)(r * Mathf.Sin(position));
-        Vector3 r_summer = new Vector3(x, y, 0);
-
-        position = (float)precession+(float)Mathf.PI; 
-        r = (float)(a * (1 - Mathf.Pow((float)eccentricity, 2)) / (1 + (float)eccentricity * Mathf.Cos(position)));
-        x = (float)(r * Mathf.Cos(position));
-        y = (float)(r * Mathf.Sin(position));
-        Vector3 r_fall = new Vector3(x, y, 0);
-
-        position = (float)precession+3*(float)Mathf.PI/2;
-        r = (float)(a * (1 - Mathf.Pow((float)eccentricity, 2)) / (1 + (float)eccentricity * Mathf.Cos(position)));
-        x = (float)(r * Mathf.Cos(position));
-        y = (float)(r * Mathf.Sin(position));
-        Vector3 r_winter = new Vector3(x, y, 0);
-
-        //Radius vector of the Earth 
-        r = (float)(a * (1 - Mathf.Pow((float)eccentricity, 2)) / (1 + (float)eccentricity * Mathf.Cos((float)position_e))); //center of coordinate system for this parametrization is the Sun, in the right focus of the ellipse.
-        x = (float)(r * Mathf.Cos(position_e));
-        y = (float)(r * Mathf.Sin(position_e));
-        Vector3 r_vector =  new Vector3(x, y, 0);
-
-
-        double radius = a*(1 - Mathf.Pow(eccentricity, 2)) / (1 + eccentricity * Mathf.Cos(position_e));
-        
-        
-        InsolationCalculator.Insolation(Fo, r_vector.Length(), latitude, -8.285, AU, out F_bar, out daylength);
-        
-        textToDisplay += "\n\nInsolation -----------------");
-        textToDisplay += "\nInsolation (TOA): " + F_bar);
-        textToDisplay += "\nDay length: " + daylength);
-        textToDisplay += "\nSun-Earth distance in km*1e+6: " + r);
-
-
-        textToDisplay += "\n\nr_spring vector: " + r_spring);
-        textToDisplay += "\nr_summer vector: " + r_summer);
-        textToDisplay += "\nr_fall vector: " + r_fall);
-        textToDisplay += "\nr_winter vector: " + r_winter);
-        textToDisplay += "\nr vector: " + r_vector);
-
-        double[,] tilt_m;
-        GenerateRot.rotation(obliquity, precession, out tilt_m);
-
-        float R = (float)0.15*(float)a; //this determines the size of Earth for plotting purposes, relative to the orbit size
-        float R_sun = 1/3*R; //size of the Sun for the plot
-
-        Vector3 k = new Vector3(0, 0, R * 1.5f);
-
-        float x1 = (float)tilt_m[0, 0] * k.X + (float)tilt_m[0, 1] * k.Y + (float)tilt_m[0, 2] * k.Z;
-        float y1 = (float)tilt_m[1, 0] * k.X + (float)tilt_m[1, 1] * k.Y + (float)tilt_m[1, 2] * k.Z;
-        float z1 = (float)tilt_m[2, 0] * k.X + (float)tilt_m[2, 1] * k.Y + (float)tilt_m[2, 2] * k.Z;
-        Vector3 kk = new Vector3(x1, y1, z1);
-
-        double solar_declination = (180/Mathf.PI) * Mathf.Acos(Vector3.Dot(r_vector, kk) / (kk.Length()*r_vector.Length())) - 90;
-        textToDisplay += "\n\nSolar declination: " + solar_declination);     
-
-
-        */
     }
     
 }
